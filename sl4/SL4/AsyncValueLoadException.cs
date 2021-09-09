ab95ac446d56d2a07f26a79bc6d78d198c5020af
Jan Ivar Z. Carlsen <jizc@outlook.com>
2021-09-09 13:48:11 +0200

    Removed Silverlight projects

--- sl4/SL4/AsyncValueLoadException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace PixelLab.SL
-{
-    public class AsyncValueLoadException : Exception
-    {
-        public AsyncValueLoadException(Exception innerException) : base("An exception was thrown while loading an async value.", innerException) { }
-    }
-}
--- sl4/SL4/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Windows.Markup;
-
-[assembly: XmlnsDefinition("http://schemas.thinkpixellab.com/2011/xaml/presentation", "PixelLab.SL")]
